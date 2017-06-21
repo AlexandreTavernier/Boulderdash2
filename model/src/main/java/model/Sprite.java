a888b1f9963c4627cb758b425cb60b94366592e1
AlexandreTavernier <alexandredidierrene.tavernier@viacesi.fr>
2017-06-21 13:54:55 +0200

    commit

--- model/src/main/java/model/Sprite.java
+++ model/src/main/java/model/Sprite.java
@@ -1,5 +1,17 @@
 package model;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 public class Sprite {
-
+	private Image image;
+	private String imageName;
+	private char consoleImage;
+	private boolean imageLoaded;
+		
+		
+	public Sprite(char character, String imageName){
+		this.setConsoleImage(character);
+		this.setImageName(imageName);
+	}
 }
