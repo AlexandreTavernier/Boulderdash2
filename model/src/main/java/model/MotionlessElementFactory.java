ed25254e66b4f3262bb9b0c0af0825bb20086bed
Efiria <bruno.doucet@viacesi.fr>
2017-06-22 13:44:45 +0200

    Class

--- model/src/main/java/model/MotionlessElementFactory.java
+++ model/src/main/java/model/MotionlessElementFactory.java
@@ -1,5 +1,12 @@
 package model;
 
 public abstract class MotionlessElementFactory  {
-	
+
+	public static MotionlessElement getFromFileSymbol(final char fileSymbol) {
+			for (final MotionlessElement motionlessElement : motionlessElements) {
+					if (motionlessElement.getSprite().getConsoleImage() == fileSymbol) {
+							return motionlessElement;
+					}
+			}
+
 }
