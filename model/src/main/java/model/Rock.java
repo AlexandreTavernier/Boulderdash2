5a473ec13abd3cfae1ea5ee080753a63570c80b6
Efiria <bruno.doucet@viacesi.fr>
2017-06-23 10:00:54 +0200

    boulder

--- model/src/main/java/model/Rock.java
+++ model/src/main/java/model/Rock.java
@@ -11,7 +11,7 @@ import Interface.ISprite;
 import Interface.Permeability;
 
 public class Rock extends Mobile {
-	private static final ISprite SPRITE = new Sprite('R', "rock.jpg");
+	private static final ISprite SPRITE = new Sprite('R', "boulder.jpg");
 
 	public Rock(final int x, final int y, final IMap map) throws IOException{
 		super(x, y, SPRITE, map, Permeability.PUSH);
